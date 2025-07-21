a5c5c2e3802bc7048dbe8c896e02d2f3e3dc1f8f
Eremia Valentin <valentineremia@yahoo.com>
2025-07-21 14:14:18 +0300

    rifresh page & glass AppBar

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "138481404"
+    serviceWorkerVersion: "220490097"
   }
 });
