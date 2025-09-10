bb79fe6f1e8bd935970e6ee025ac7f84233cc7da
Eremia Valentin <valentineremia@yahoo.com>
2025-09-10 20:32:14 +0300

    laucher icon change

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"cf56914b326edb0ccb123ffdc60f00060bd513
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "3503816759"
+    serviceWorkerVersion: "3758904514"
   }
 });
